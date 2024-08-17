5eb9e7e36a29b2252a48e6a7c7e8d5f431761df8
svbiz4u <136245961+svbiz4u@users.noreply.github.com>
2024-08-17 12:09:10 +0200

    com-17082024-1209

--- js/task.js
+++ js/task.js
@@ -1,4 +1,53 @@
-'03-08-24'
+// 17/08/2024
+
+//Напишіть дві функції
+//makeProduct(name, price, callback) - приймає
+//ім'я та ціну товару, а також callback.
+//Функція створює об'єкт товару, додаючи йому унікальний
+//ідентифікатор як id і викликає callback
+//Передаючи йому створений об'єкт.
+//showProduct(product) - коллбек приймаючий об'єкт
+//продукту і логірующий їх у консоль
+
+
+// const makeProduct =(name, price, callback) => {
+// const product = {
+//   name, 
+//   price,
+//   id: Math.random() }
+//   callback(product)
+// }
+// const showProduct = (productName) => {
+// console.log(productName)  
+// }
+// makeProduct ("Iphone", 1000, showProduct)
+
+
+
+//Напишіть дві функції
+// letMeSeeYourName(callback) - запитує ім'я користувача
+//через prompt та викликає callback функцію
+//greet(name) - коллбек, що приймає ім'я і логірує в консоль
+//Рядок "Привіт <name>"
+//Реалізуй перевірку, що prompt не порожній
+
+// const letMeSeeYourName =  (callback) => {
+// const userName = prompt('Enter your name')
+// if (userName)
+// callback (!userName) {
+//   alert('not correct ')
+//   return;
+// };
+// }
+// const  greet = (name) => {
+// console.log (`Привіт ${name}`)
+// }
+// console.log(prompt('Enter your name'))
+// letMeSeeYourName(greet);
+
+
+
+// '03-08-24'
 // Створіть масив styles з елементами «Джаз» та «Блюз».
 //Додайте "Рок-н-рол" в кінець.
 // Замініть значення "Блюз" на "Класика".
